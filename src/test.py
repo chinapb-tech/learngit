19f18fb7e3bd99f2796a4eed33fea66c01096ab2
pengbo16 <pengbo16@hikcreate.com>
2021-10-11 15:01:04 +0800

    新增文件

---
+++ src/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/11 15:00
+# @Author  : pengbo
+# @FileName: test.py
+# @Version : 1.0.0
+# @Software: PyCharm
---
+++ ss/fis.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/11 14:59
+# @Author  : pengbo
+# @FileName: fis.py
+# @Version : 1.0.0
+# @Software: PyCharm
